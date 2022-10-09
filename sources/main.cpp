ceefbe9976ab36e683d53b73cf394a4288e05e69
Dumble009 <daichi19990821@gmail.com>
2022-10-09 15:41:33 +0900

    env: FBXSDKをインクルードし、FbxManagerオブジェクトを生成、破棄するコードを含んだmain関数をビルド・実行することが出来る。

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,15 @@
-// ----------STLのinclude----------
-#include <iostream> // 入出力
+#include <iostream>
+#include <fbxsdk.h>
 
 int main(void)
 {
-    std::cout << "Hello World!" << std::endl;
+    fbxsdk::FbxManager *fbx_manager = fbxsdk::FbxManager::Create();
+
+    std::cout << "create FbxManager" << std::endl;
+
+    fbx_manager->Destroy();
+
+    std::cout << "destroy FbxManager" << std::endl;
+
+    return 0;
 }
